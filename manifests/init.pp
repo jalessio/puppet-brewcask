458df853f1c6d650f4960cb0fcc5d6a36e3e0bbf
Jamie <jamie@stoic.net>
2013-07-16 17:01:32 -0700

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,12 @@
 class brewcask {
   require homebrew
 
-  exec { 
-    'tap-homebrew-cask':
-      command => 'brew tap phinze/homebrew-cask',
-      creates => "${homebrew::config::installdir}/Library/Taps/phinze-cask",
-    'homebrew-cask':
-      command => 'brew cask'
+  exec { 'tap-homebrew-cask':
+    command => 'brew tap phinze/homebrew-cask && brew cask',
+    creates => "${homebrew::config::installdir}/Library/Taps/phinze-cask",
   }
 
   package { 'brew-cask':
-    require => Exec['tap-homebrew-cask', 'homebrew-cask']
+    require => Exec['tap-homebrew-cask']
   }
 }
